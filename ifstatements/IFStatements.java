db3d3623f5231e71e11ff7b44b14fe78d800b358
unknown <25019625@mvula.univen.ac.za>
2025-05-08 22:02:46 +0200

    first commit

---
+++ ifstatements/IFStatements.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ifstatements;
+
+/**
+ *
+ * @author admin
+ */
+public class IFStatements {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+int user = 18;
+if (user<=18){
+
+System.out.println("The user is 18 or younger");
+    }
+  if (user > 18){
+System.out.println("The user is older tha 18 ");
+}
+}
+}
